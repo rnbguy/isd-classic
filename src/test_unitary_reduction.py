25d7eca8a62836cae1fad2830eff3b3b72eacbe6
Simone Perriello <simone.perriello@protonmail.com>
2018-10-27 15:32:53 +0200

    improved unitary reduction functions
    - Added separate test-cases
    - Added options to get the RREF unitary matrix in the right part (fundamental for parity matrices)

---
+++ src/test_unitary_reduction.py
@@ -0,0 +1,147 @@
+import numpy as np
+import logging
+import utils
+import hamming_code
+
+
+# Only works if the mod2 operation is done at the end
+# TODO find a way to force mod2 operation in numpy
+def test_parity_matrix(n, k, startAtEnd=False):
+    h = hamming_code.get_parity_matrix_nonsystematic(n, k)
+    sum_h = np.sum(h)
+    _logger.debug("TESTING PARITY MATRIX ({0}, {1}), {2},\nH = \n{2}".format(
+        n, k, startAtEnd, 1 * h))
+    _logger.debug("Overall matrix sum is {0}".format(sum_h))
+
+    p, l, u = utils.get_rref(h, 3, startAtEnd=startAtEnd)
+    # The idea is that if all items are None, then the algorithm wasn't able to
+    # find a proper unitary rref submatrix, so we shuffle again the columns
+    # of the original matrix
+    while (all(item is None for item in (p, l, u))):
+        h = np.random.permutation(h.T).T
+        _logger.debug("*********** PERMUTED, new H = \n{0}".format(1 * h))
+        _logger.debug("Overall matrix sum is {0}".format(np.sum(h)))
+        p, l, u = utils.get_rref(h, 3, startAtEnd=startAtEnd)
+
+    u = u % 2
+    l = l % 2
+    _logger.debug("H=\n{0}\nH_RREF=\n{1}\nLTOT=\n{2}\n".format(
+        1 * h, 1 * u, 1 * l))
+    _logger.debug("Overall matrix sum is {0}".format(np.sum(u)))
+
+    lh = np.dot(l, h) % 2
+    id = np.eye(n - k)
+    square_matrix_start = 0 if not startAtEnd else k
+    square_matrix_end = n - k if not startAtEnd else n
+    _logger.debug("Slicing with startAtEnd={2}, start={0}, end={1}".format(
+        square_matrix_start, square_matrix_end, startAtEnd))
+
+    # Take the right-most n-k square matrix, i.e.:
+    # - all the rows (:)
+    # - columns from start to end
+    tst = u[:, square_matrix_start:square_matrix_end]
+    _logger.debug("Testing L * H, should be equal to H_RREF\n{0}".format(
+        1 * lh))
+    _logger.debug("Asserting equalities...")
+    np.testing.assert_almost_equal(tst, id)
+    np.testing.assert_almost_equal(lh, u)
+    np.testing.assert_equal(sum_h, np.sum(u))
+    _logger.debug("...OK")
+
+
+def test_zero():
+    _logger.debug("***test zero")
+    a = np.array([[0, 1, 1], [3, 1, 1], [8, 7, 9]], dtype=np.float)
+    exp_res = np.eye(3)
+    p, l, u = utils.get_rref(a, 3, dtype=np.float)
+    lta = np.dot(l, a)
+
+    _logger.debug("A=\n{0}\nA_RREF=\n{1}\nLTOT=\n{2}\nLTOT*A=\n{3}".format(
+        a, u, l, lta))
+    _logger.debug("Asserting equalities...")
+    np.testing.assert_almost_equal(u, exp_res)
+    np.testing.assert_almost_equal(lta, u)
+    _logger.debug("...OK")
+
+    # tst = np.dot(l.T, u)
+    # _logger.debug("Testing L.T * U, should be equal to A\n{0}".format(tst))
+    # tst = np.dot(p, a)
+    # _logger.debug("Testing P * A, should be equal to A\n{0}".format(tst))
+
+
+def test_pdf():
+    _logger.debug("***test pdf")
+    _logger.debug("***stopAt 1***")
+    a = np.array([[2, 1, 1, 0], [4, 3, 3, 1], [8, 7, 9, 5], [6, 7, 9, 8]],
+                 dtype=np.float)
+    exp_res = np.array([[2, 1, 1, 0], [0, 1, 1, 1], [0, 0, 2, 2], [0, 0, 0,
+                                                                   2]])
+    p, l, u = utils.get_rref(a, 1)
+    tst = np.dot(l, a)
+    _logger.debug("A=\n{0}\nA_RREF=\n{1}\nLTOT=\n{2}\nLTOT*A=\n{3}".format(
+        a, u, l, tst))
+
+    _logger.debug("Asserting equalities for stopAt 1...")
+    np.testing.assert_almost_equal(u, exp_res)
+    np.testing.assert_almost_equal(tst, u)
+    _logger.debug("...OK")
+
+    _logger.debug("***stopAt 3***")
+    p, l, u = utils.get_rref(a, 3)
+    exp_res = np.eye(4)
+    tst = np.dot(l, a)
+    _logger.debug("A=\n{0}\nA_RREF=\n{1}\nLTOT=\n{2}\nLTOT*A=\n{3}".format(
+        a, u, l, tst))
+
+    _logger.debug("Asserting equalities for stopAt 1...")
+    np.testing.assert_almost_equal(u, exp_res)
+    np.testing.assert_almost_equal(tst, u)
+    _logger.debug("...ok")
+
+
+def test_wiki(startAtEnd=False):
+    _logger.debug("***test wiki")
+    a = np.array([[2, 1, -1, 8], [-3, -1, 2, -11], [-2, 1, 2, -3]])
+    exp_res = np.array([[1, 0, 0, 2], [0, 1, 0, 3], [0, 0, 1, -1]])
+
+    ptot, ltot, u = utils.get_rref(a, startAtEnd=startAtEnd)
+    lta = np.dot(ltot, a)
+
+    _logger.debug("A = \n{0}\nA_RREF=\n{1}\nLTOT=\n{2}\nLTOT*A=\n{3}".format(
+        a, u, ltot, lta))
+    _logger.debug("Asserting equalities...")
+    np.testing.assert_almost_equal(u, exp_res)
+    np.testing.assert_almost_equal(lta, u)
+    _logger.debug("...OK")
+
+
+def test_sympy_rref(h):
+    import sympy
+    m = sympy.Matrix(h).rref()
+    _logger.debug(m)
+
+
+def main():
+    test_wiki()
+    # test_wiki(True)
+    test_pdf()
+    test_zero()
+    test_parity_matrix(7, 4)
+    test_parity_matrix(7, 4, True)
+    test_parity_matrix(15, 11)
+    test_parity_matrix(15, 11, True)
+
+
+if __name__ == "__main__":
+    global _logger
+    _logger = logging.getLogger(__name__)
+    _handler = logging.StreamHandler()
+    _formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    _handler.setFormatter(_formatter)
+    if (_logger.hasHandlers()):
+        _logger.handlers.clear()
+    _logger.addHandler(_handler)
+    _logger.setLevel(logging.DEBUG)
+
+    main()
---
+++ src/utils.py
@@ -0,0 +1,177 @@
+import numpy as np
+import logging
+
+_logger = logging.getLogger(__name__)
+_handler = logging.StreamHandler()
+_formatter = logging.Formatter(
+    '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+_handler.setFormatter(_formatter)
+if (_logger.hasHandlers()):
+    _logger.handlers.clear()
+_logger.addHandler(_handler)
+_logger.setLevel(logging.DEBUG)
+
+
+# It uses a sort of LPU decomposition, where we're not really interested in
+# L and P separately, but as a whole matrix applied to the matrix to obtain the
+# RREF.
+# I.e., given the original matrix M, we want to obtain its RREF form U and the
+# corresponding L matrix s.t. L*M = U.
+def get_rref(m, stopAt=3, dtype=np.float, startAtEnd=False):
+    """FIXME! briefly describe function
+
+    :param m: The matrix to be reduced
+    :param stopAt: 1 if we want only the REF form, 2 if we want the RREF form, 3 (default) if we want the unitary RREF
+    :param dtype: the dtype of the array (default np.float)
+    :param startAtEnd: You want to leave it at False for most of the cases. It is set to True to have the systematic form of the parity matrix, where the unitary RREF should be in the last positions (i.e. H = [ A | I_{square - square_start}], where I is the identity matrix, i.e. our unitary RREF)
+    :returns: (L, P, U): L the transformation matrix s.t. L*M = U; U is the reduced matrix; P is the permutation matrix for the rows (useless atm). (None, None, None) if reduction is not possible.
+    :rtype: tuple of np.array
+
+    """
+    _logger.debug("M = \n{0}".format(1 * m))
+    u = m
+    # This is used bcz the matrix is not surely a square matrix, so
+    # we're going to consider the maximum b/w number of rows and columns as the
+    # size of the square matrix.
+    square = u.shape[0] if (u.shape[0] < u.shape[1]) else u.shape[1]
+
+    _logger.debug("square = {0}".format(square))
+
+    # If startAtEnd is set, the square matrix should be constructed in the right part of the matrix
+    square_start = u.shape[1] - square if (startAtEnd) else 0
+    _logger.debug("Square start is = {0}".format(square_start))
+
+    # ltot will cointain all the transformations done on the original matrix
+    ltot = np.eye(square, dtype=dtype)
+    # ptot should cointain all the row permutation done to the original matrix
+    # It's really useless in our functions, but we track it just for future reference
+    ptot = np.eye(square, dtype=dtype)
+
+    if (stopAt < 1 or stopAt > 3):
+        exit("Error stopAt")
+    _logger.debug("*** PART 1: Row Echelon Form")
+    ptot, ltot, u = _ref(u, square, square_start, ltot, ptot, dtype)
+    _logger.debug("*** PART 1: Completed")
+    if (stopAt == 1):
+        return (ptot, ltot, u)
+    if (all(item is None for item in (ptot, ltot, u))):
+        _logger.debug("None tuple returned")
+        return (None, None, None)
+
+    _logger.debug("*** PART 2: Reduced Row Echelon Form *********")
+    ptot, ltot, u = _rref(u, square, square_start, ltot, ptot, dtype)
+    _logger.debug("*** PART 2: Completed")
+    if (stopAt == 2):
+        return (ptot, ltot, u)
+    if ((ptot, ltot, u) is (None, None, None)):
+        _logger.debug("None type returned")
+        return (None, None, None)
+
+    _logger.debug("*** PART 3: Normalizing coeffients on diagonal *********")
+    ptot, ltot, u = _normalize(u, square, square_start, ltot, ptot, dtype)
+    _logger.debug("*** PART 3: Completed")
+    return (ptot, ltot, u)
+
+
+def _ref(u, square, square_start, ltot, ptot, dtype):
+    for i in range(square_start, square_start + square):  # columns, 0.. 3
+        l = np.eye(square, dtype=dtype)
+        p = np.eye(square, dtype=dtype)
+
+        # Checking if there are 0's on the diagonal, if so try to swap the row
+        # containing the zero with one of the rows below it
+        exc_row = i + 1
+        while (u[i - square_start][i] == 0):
+            _logger.debug("0 at i = {0}".format(i))
+            # revert back u and p at their original states at each iteration
+            # First time it's useless
+            u = np.dot(p.T, u)
+            p = np.dot(p.T, p)
+
+            if (exc_row >= square):
+                # In this case, we've been unable to swap rows
+                _logger.debug(
+                    "Unable to swap rows, returning (None, None, None)")
+                return (None, None, None)
+            # swap rows
+            # The first is a random solution
+            # p = np.random.permutation(p)
+            # u = np.dot(p, u)
+            # However, we should only swap the ones below i
+            p[[i, exc_row]] = p[[exc_row, i]]
+            u = np.dot(p, u)
+            exc_row += 1
+            _logger.debug("Permuted U = \n{0}\n".format(1 * u))
+
+        ptot = np.dot(p, ptot)
+        ltot = np.dot(p, ltot)
+
+        for j in range(i - square_start + 1, square):  #rows, i+1 .. 3
+            _logger.debug("i = {0}, j = {1}".format(i, j))
+            l[j][i - square_start] = -(u[j][i] / u[i - square_start][i])
+        ltot = np.dot(l, ltot)
+        u = np.dot(l, u)
+        _logger.debug("\nL = \n{0}\nU =\n{1}".format(1 * l, 1 * u))
+
+    return (ptot, ltot, u)
+
+
+def _rref(u, square, square_start, ltot, ptot, dtype):
+    for i in range(square_start + square - 1, square_start,
+                   -1):  # columns, 2..1
+        l = np.eye(square, dtype=dtype)
+        p = np.eye(square, dtype=dtype)
+
+        exc_row = i + 1
+        while (u[i - square_start][i] == 0):
+            _logger.debug("0 at i = {0}".format(i))
+            # revert back u and p at their original states at each iteration
+            # First time it's useless
+            u = np.dot(p.T, u)
+            p = np.dot(p.T, p)
+
+            if (exc_row == square - 1):
+                exit("Error, unable to swap rows")
+            # swap rows
+            # Random solution, but we should only swap the ones below i
+            # p = np.random.permutation(p)
+            # u = np.dot(p, u)
+            p[[i, exc_row]] = p[[exc_row, i]]
+            u = np.dot(p, u)
+            exc_row += 1
+            _logger.debug("Permuted U = \n{0}\n".format(1 * u))
+
+        ptot = np.dot(p, ptot)
+        ltot = np.dot(p, ltot)
+
+        # i = 6, 5, 4
+        # j = (1, 0), (0)
+        # i = 2, 1, 0
+        # j = (1, 0), (0)
+        for j in range(i - square_start - 1, -1, -1):  # i-1..0
+            _logger.debug("i = {0}, j = {1}".format(i, j))
+            l[j][i - square_start] = -(u[j][i] / u[i - square_start][i])
+        ltot = np.dot(l, ltot)
+        u = np.dot(l, u)
+        _logger.debug("\nL = \n{0}\nU =\n{1}".format(1 * l, 1 * u))
+
+    return (ptot, ltot, u)
+
+
+def _normalize(u, square, square_start, ltot, ptot, dtype):
+    l = np.eye(square, dtype=dtype)
+    for i in range(square_start, square_start + square):
+        _logger.debug("i = {0}".format(i))
+        if (u[i - square_start][i] == 0):
+            return (None, None, None)
+        if (u[i - square_start][i] != 1):
+            _logger.debug("{0} != 1".format(u[i - square_start][i]))
+            l[i - square_start][i - square_start] = 1 / u[i - square_start][i]
+            _logger.debug("l[i][i] = {0}".format(
+                l[i - square_start][i - square_start]))
+    u = np.dot(l, u)
+    _logger.debug("\nL = \n{0}\nU =\n{1}".format(1 * l, 1 * u))
+    ltot = np.dot(l, ltot)
+    # _logger.debug("\nL = \n{0}\nU =\n{1}\nLTOT =\n{2}".format(l, u, ltot))
+
+    return (ptot, ltot, u)
