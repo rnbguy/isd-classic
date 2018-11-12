cb9e51f7cc44a38f9b3c2832264ee99bcc272d48
Simone Perriello <simone.perriello@protonmail.com>
2018-11-12 18:48:42 +0100

    utils: removed dtype, added modulo operations
    dtype is useless in our algorithm: we need float.
    mod is used to allow modulo operations (f.e. mod2, i.e. binary operations)

--- src/utils.py
+++ src/utils.py
@@ -17,24 +17,24 @@ _logger.setLevel(logging.DEBUG)
 # RREF.
 # I.e., given the original matrix M, we want to obtain its RREF form U and the
 # corresponding L matrix s.t. L*M = U.
-def get_rref(m, stopAt=3, dtype=np.float, startAtEnd=False):
+def get_rref(m, stopAt=3, startAtEnd=False, mod=None):
     """FIXME! briefly describe function
 
     :param m: The matrix to be reduced
     :param stopAt: 1 if we want only the REF form, 2 if we want the RREF form, 3 (default) if we want the unitary RREF
-    :param dtype: the dtype of the array (default np.float)
     :param startAtEnd: You want to leave it at False for most of the cases. It is set to True to have the systematic form of the parity matrix H, where the unitary RREF should be in the last positions (i.e. H = [ A | I_{square - square_start}], where I is the identity matrix, i.e. our unitary RREF)
+    :param mod: Used when we want to do modulo operations, i.e. all elements of the matrices are modulo mod
     :returns: (P, L, U): L the transformation matrix s.t. L*M = U; U is the reduced matrix; P is the permutation matrix for the rows (useless atm). (None, None, None) if reduction is not possible.
     :rtype: tuple of np.array
 
     """
-    # Multiplied by 1 bcz if dtype is bool, in this way we show a matrix of 
-    # 0's and 1's instead of True and False
-    _logger.debug("M = \n{0}".format(1 * m))
+    _logger.debug("Starting RREF algorithm")
+    _logger.debug("Original matrix is M = \n{0}".format(m))
     # Make a copy of the original matrix, just in case it may serve.
     # Maybe it's useless, but since python passes references to objects by values
     # (i.e. like in Java) https://stackoverflow.com/a/986145/2326627
-    u = m
+    # Do modulo in case the original matrix is not yet in modulo form
+    u = m if mod is None else np.mod(m, mod)
     # This is used bcz the matrix is not surely a square matrix, so
     # we're going to consider the maximum b/w number of rows and columns as the
     # size of the square matrix.
@@ -48,15 +48,16 @@ def get_rref(m, stopAt=3, dtype=np.float, startAtEnd=False):
 
     # ltot will cointain all the transformations done on the original matrix
     # At the beginning, it's obv an identity matrix
-    ltot = np.eye(square, dtype=dtype)
+    ltot = np.eye(square)
     # ptot should cointain all the row permutation done to the original matrix
     # It's really useless in our functions, but we track it just for future reference
-    ptot = np.eye(square, dtype=dtype)
+    ptot = np.eye(square)
 
     if (stopAt < 1 or stopAt > 3):
         exit("Error stopAt")
+
     _logger.debug("*** PART 1: Row Echelon Form")
-    ptot, ltot, u = _ref(u, square, square_start, ltot, ptot, dtype)
+    ptot, ltot, u = _ref(u, square, square_start, ltot, ptot, mod)
     _logger.debug("*** PART 1: Completed")
     if (stopAt == 1):
         return (ptot, ltot, u)
@@ -65,7 +66,7 @@ def get_rref(m, stopAt=3, dtype=np.float, startAtEnd=False):
         return (None, None, None)
 
     _logger.debug("*** PART 2: Reduced Row Echelon Form *********")
-    ptot, ltot, u = _rref(u, square, square_start, ltot, ptot, dtype)
+    ptot, ltot, u = _rref(u, square, square_start, ltot, ptot, mod)
     _logger.debug("*** PART 2: Completed")
     if (stopAt == 2):
         return (ptot, ltot, u)
@@ -74,23 +75,25 @@ def get_rref(m, stopAt=3, dtype=np.float, startAtEnd=False):
         return (None, None, None)
 
     _logger.debug("*** PART 3: Normalizing coeffients on diagonal *********")
-    ptot, ltot, u = _normalize(u, square, square_start, ltot, ptot, dtype)
+    ptot, ltot, u = _normalize(u, square, square_start, ltot, ptot, mod)
     _logger.debug("*** PART 3: Completed")
-    _logger.debug("***\nLTOT=\n{0}\nU=\n{1}".format(ltot, u))
+    _logger.debug("***\nPTOT\n{0}\nLTOT=\n{1}\nU=\n{2}".format(ptot, ltot, u))
     return (ptot, ltot, u)
 
 
 # A REF is a triangular matrix with all 0's below the diagonal
-def _ref(u, square, square_start, ltot, ptot, dtype):
+def _ref(u, square, square_start, ltot, ptot, mod):
     for i in range(square_start, square_start + square):  # columns, 0.. 3
-        l = np.eye(square, dtype=dtype)
-        p = np.eye(square, dtype=dtype)
+        l = np.eye(square)
+        p = np.eye(square)
 
         # Checking if there are 0's on the diagonal, if so try to swap the row
         # containing the zero with one of the rows below it
         exc_row = i + 1
         while (u[i - square_start][i] == 0):
-            _logger.debug("0 at i = {0}".format(i))
+            _logger.debug(
+                "found a 0 on diagonal at i,j = {0}, trying to swap rows {0} with one of the rows below"
+                .format(i))
             # revert back u and p at their original states at each iteration
             # First time it's useless
             u = np.dot(p.T, u)
@@ -109,28 +112,40 @@ def _ref(u, square, square_start, ltot, ptot, dtype):
             p[[i, exc_row]] = p[[exc_row, i]]
             u = np.dot(p, u)
             exc_row += 1
-            _logger.debug("Permuted U = \n{0}\n".format(1 * u))
+            _logger.debug("Permuted U = \n{0}\n".format(u))
 
+        # Permutation matrix, contains only 1, no need to do mod operations
         ptot = np.dot(p, ptot)
         ltot = np.dot(p, ltot)
 
         for j in range(i - square_start + 1, square):  #rows, i+1 .. 3
             _logger.debug("i = {0}, j = {1}".format(i, j))
-            l[j][i - square_start] = -(u[j][i] / u[i - square_start][i])
+            _logger.debug(
+                "L[{0}, {2}] is equal to -U[{0}, {1}]/U[{2}, {1}]".format(
+                    j, i, i - square_start))
+            l_entry = -(u[j][i] / u[i - square_start][i])
+            if mod is not None:
+                l_entry = l_entry % mod
+            _logger.debug("In practice, L[{0}, {1}] is equal to {2}".format(
+                j, i - square_start, l_entry))
+            l[j][i - square_start] = l_entry
         ltot = np.dot(l, ltot)
         u = np.dot(l, u)
-        _logger.debug("\nL = \n{0}\nU =\n{1}".format(1 * l, 1 * u))
+        if mod is not None:
+            ltot = np.mod(ltot, 2)
+            u = np.mod(u, 2)
+        _logger.debug("\nL = \n{0}\nU =\n{1}".format(l, u))
 
     return (ptot, ltot, u)
 
 
 # A RREF is a triangular matrix with all 0's below and above the diagonal
 # (i.e. all non-zero values are on the diagonal)
-def _rref(u, square, square_start, ltot, ptot, dtype):
+def _rref(u, square, square_start, ltot, ptot, mod):
     for i in range(square_start + square - 1, square_start,
                    -1):  # columns, 2..1
-        l = np.eye(square, dtype=dtype)
-        p = np.eye(square, dtype=dtype)
+        l = np.eye(square)
+        p = np.eye(square)
 
         exc_row = i + 1
         while (u[i - square_start][i] == 0):
@@ -147,10 +162,13 @@ def _rref(u, square, square_start, ltot, ptot, dtype):
             # p = np.random.permutation(p)
             # u = np.dot(p, u)
             p[[i, exc_row]] = p[[exc_row, i]]
+            _logger.debug(
+                "The applied permutation matrix is = \n{0}\n".format(p))
             u = np.dot(p, u)
             exc_row += 1
-            _logger.debug("Permuted U = \n{0}\n".format(1 * u))
+            _logger.debug("Permuted U = \n{0}\n".format(u))
 
+        # Permutation matrix, contains only 1, no need to do mod operations
         ptot = np.dot(p, ptot)
         ltot = np.dot(p, ltot)
 
@@ -160,28 +178,36 @@ def _rref(u, square, square_start, ltot, ptot, dtype):
         # j = (1, 0), (0)
         for j in range(i - square_start - 1, -1, -1):  # i-1..0
             _logger.debug("i = {0}, j = {1}".format(i, j))
-            l[j][i - square_start] = -(u[j][i] / u[i - square_start][i])
+            l_entry = -(u[j][i] / u[i - square_start][i])
+            if mod is not None:
+                l_entry = l_entry % mod
+            l[j][i - square_start] = l_entry
         ltot = np.dot(l, ltot)
         u = np.dot(l, u)
-        _logger.debug("\nL = \n{0}\nU =\n{1}".format(1 * l, 1 * u))
+        if mod is not None:
+            ltot = np.mod(ltot, 2)
+            u = np.mod(u, 2)
+        _logger.debug("\nL = \n{0}\nU =\n{1}".format(l, u))
 
     return (ptot, ltot, u)
 
 
 # We put all the non-zero entries of the diagonal to 1
-def _normalize(u, square, square_start, ltot, ptot, dtype):
-    l = np.eye(square, dtype=dtype)
+# At this point, it shouldn't be necessary to do mod operation, isn't it?
+def _normalize(u, square, square_start, ltot, ptot, mod):
+    l = np.eye(square)
     for i in range(square_start, square_start + square):
         _logger.debug("i = {0}".format(i))
         if (u[i - square_start][i] == 0):
             return (None, None, None)
         if (u[i - square_start][i] != 1):
-            _logger.debug("{0} != 1".format(u[i - square_start][i]))
+            _logger.debug("diagonal element at {0},{0} != 1".format(
+                u[i - square_start][i]))
             l[i - square_start][i - square_start] = 1 / u[i - square_start][i]
             _logger.debug("l[i][i] = {0}".format(
                 l[i - square_start][i - square_start]))
     u = np.dot(l, u)
-    _logger.debug("\nL = \n{0}\nU =\n{1}".format(1 * l, 1 * u))
+    _logger.debug("\nL = \n{0}\nU =\n{1}".format(l, u))
     ltot = np.dot(l, ltot)
     # _logger.debug("\nL = \n{0}\nU =\n{1}\nLTOT =\n{2}".format(l, u, ltot))
 
--- src/utils_test.py
+++ src/utils_test.py
@@ -4,32 +4,34 @@ import utils
 import hamming_codes as hc
 
 
-# Only works if the mod2 operation is done on the results of the computation
-# ATM, it seems that np.array can't be forced to do mod2 operations (even w/ dtype=bool)
 def test_parity_matrix(n, k, startAtEnd=False):
     h = hc.get_parity_matrix_nonsystematic(n, k)
+    # Sum of ones of H, just a quick way to check that all the transformations
+    # applied doesn't modify the overall structure of H
     sum_h = np.sum(h)
-    _logger.debug("TESTING PARITY MATRIX ({0}, {1}), {2},\nH = \n{2}".format(
-        n, k, startAtEnd, 1 * h))
+    _logger.debug(
+        "TESTING PARITY MATRIX (n = {0}, k = {1}), startAtEnd = {2},\nH = \n{3}"
+        .format(n, k, startAtEnd, h))
     _logger.debug("Overall matrix sum is {0}".format(sum_h))
 
-    p, l, u = utils.get_rref(h, 3, startAtEnd=startAtEnd)
+    # h_rref is the original H in rref, l is the transformation matrix applied
+    # overall to obtain u from H
+    _, l, h_rref = utils.get_rref(h, 3, startAtEnd=startAtEnd, mod=2)
     # The idea is that if all items are None, then the algorithm wasn't able to
     # find a proper unitary rref submatrix, so we shuffle again the columns
     # of the original matrix
-    while (all(item is None for item in (p, l, u))):
+    while (all(item is None for item in (l, h_rref))):
         h = np.random.permutation(h.T).T
-        _logger.debug("*********** PERMUTED, new H = \n{0}".format(1 * h))
-        _logger.debug("Overall matrix sum is {0}".format(np.sum(h)))
-        p, l, u = utils.get_rref(h, 3, startAtEnd=startAtEnd)
+        _logger.debug("*********** PERMUTED, new H = \n{0}".format(h))
+        _logger.debug("Overall matrix H sum (number of ones) is {0}".format(
+            np.sum(h)))
+        _, l, h_rref = utils.get_rref(h, 3, startAtEnd=startAtEnd, mod=2)
 
-    u = u % 2
-    l = l % 2
-    _logger.debug("H=\n{0}\nH_RREF=\n{1}\nLTOT=\n{2}\n".format(
-        1 * h, 1 * u, 1 * l))
-    _logger.debug("Overall matrix sum is {0}".format(np.sum(u)))
+    # L*H matrix, should be equal to the original matrix if all went good
+    lh = np.mod(np.dot(l, h), 2)
+    _logger.debug("H=\n{0}\nH_RREF=\n{1}\nLTOT=\n{2}\n".format(h, h_rref, l))
+    _logger.debug("Overall matrix sum is {0}".format(np.sum(h_rref)))
 
-    lh = np.dot(l, h) % 2
     id = np.eye(n - k)
     square_matrix_start = 0 if not startAtEnd else k
     square_matrix_end = n - k if not startAtEnd else n
@@ -39,21 +41,22 @@ def test_parity_matrix(n, k, startAtEnd=False):
     # Take the right-most n-k square matrix, i.e.:
     # - all the rows (:)
     # - columns from start to end
-    tst = u[:, square_matrix_start:square_matrix_end]
-    _logger.debug("Testing L * H, should be equal to H_RREF\n{0}".format(
-        1 * lh))
-    _logger.debug("Asserting equalities...")
+    tst = h_rref[:, square_matrix_start:square_matrix_end]
+    _logger.debug("Asserting L * H, should be equal to H_RREF")
     np.testing.assert_almost_equal(tst, id)
-    np.testing.assert_almost_equal(lh, u)
-    np.testing.assert_equal(sum_h, np.sum(u))
     _logger.debug("...OK")
+    _logger.debug("Asserting L * H, should be equal to U")
+    np.testing.assert_almost_equal(lh, h_rref)
+    _logger.debug("...OK")
+    _logger.debug("Asserting number of ones in h and u is equal".format(lh))
+    np.testing.assert_equal(sum_h, np.sum(h_rref))
 
 
 def test_zero():
     _logger.debug("***test zero")
-    a = np.array([[0, 1, 1], [3, 1, 1], [8, 7, 9]], dtype=np.float)
+    a = np.array([[0, 1, 1], [3, 1, 1], [8, 7, 9]])
     exp_res = np.eye(3)
-    p, l, u = utils.get_rref(a, 3, dtype=np.float)
+    p, l, u = utils.get_rref(a, 3)
     lta = np.dot(l, a)
 
     _logger.debug("A=\n{0}\nA_RREF=\n{1}\nLTOT=\n{2}\nLTOT*A=\n{3}".format(
@@ -67,8 +70,7 @@ def test_zero():
 def test_pdf():
     _logger.debug("***test pdf")
     _logger.debug("***stopAt 1***")
-    a = np.array([[2, 1, 1, 0], [4, 3, 3, 1], [8, 7, 9, 5], [6, 7, 9, 8]],
-                 dtype=np.float)
+    a = np.array([[2, 1, 1, 0], [4, 3, 3, 1], [8, 7, 9, 5], [6, 7, 9, 8]])
     exp_res = np.array([[2, 1, 1, 0], [0, 1, 1, 1], [0, 0, 2, 2], [0, 0, 0,
                                                                    2]])
     p, l, u = utils.get_rref(a, 1)
@@ -129,7 +131,7 @@ _handler.setFormatter(_formatter)
 if (_logger.hasHandlers()):
     _logger.handlers.clear()
 _logger.addHandler(_handler)
-_logger.setLevel(logging.ERROR)
+_logger.setLevel(logging.DEBUG)
 
 if __name__ == "__main__":
     main()
