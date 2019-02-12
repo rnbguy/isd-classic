9f3678488d9b506c525d4ec2ffae057de6580112
Simone Perriello <simone.perriello@protonmail.com>
2019-02-12 21:31:33 +0100

    Added setup.py to distribute package

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+requirements = [
+    "numpy>=1.15",
+]
+
+setup(
+    name="isd",
+    version="0.1.0",
+    description="Software that implements a few ISD algorithms",
+    url="https://github.com/tigerjack/isd-prange",
+    author="tigerjack",
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords="isd prange",
+    packages=find_packages(exclude=['test*']),
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires=">=3.5",
+)
