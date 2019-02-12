ef3be32134af4e8e11d45d436f3ea5029b7692a7
Simone Perriello <simone.perriello@protonmail.com>
2019-02-12 21:17:48 +0100

    Added methods to scramble matrix in tests

--- test/test_bruteforce.py
+++ test/test_bruteforce.py
@@ -8,18 +8,23 @@ from isd.utils import rectangular_codes_hardcoded
 
 class ISDBruteforceTest(ISDTest):
     @parameterized.expand([
-        ("n7_k4_d3_w1", 7, 4, 3, 1),
-        ("n15_k11_d4_w1", 15, 11, 4, 1),
+        ("n7_k4_d3_w1", 7, 4, 3, 1, True),
+        ("n15_k11_d4_w1", 15, 11, 4, 1, True),
+        ("n7_k4_d3_w1", 7, 4, 3, 1, False),
     ])
-    def test_h_s_d_w(self, name, n, k, d, w):
+    def test_h_s_d_w(self, name, n, k, d, w, scramble):
         # first _ is the G, we are not interested in it
         # second _ is the isHamming boolean value, not interested
-        h, _, syndromes, errors, w, _ = rectangular_codes_hardcoded.get_isd_parameters(
+        h, _, syndromes, errors, w, _ = rectangular_codes_hardcoded.get_isd_systematic_parameters(
             n, k, d, w)
         self.logger.debug("h = \n{0}".format(h))
-        p = np.random.permutation(np.eye(h.shape[1]))
-        h_p = np.dot(h, p)
-        errors_p = np.dot(errors, p)
+        if (scramble):
+            p = np.random.permutation(np.eye(h.shape[1]))
+            h_p = np.dot(h, p)
+            errors_p = np.dot(errors, p)
+        else:
+            h_p = h
+            errors_p = errors
         for i, s in enumerate(syndromes):
             with self.subTest(h=h_p, s=s, w=w):
                 self.logger.debug("Launching prange with s = {0}".format(s))
