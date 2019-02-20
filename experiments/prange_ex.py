1bf8eeb6aa58a885dd51d9bb12f5dd99f536c7aa
Simone Perriello <simone.perriello@protonmail.com>
2019-02-20 14:51:25 +0100

    Renamed prange experiment, adjusted imports

--- experiments/prange_ex.py
+++ experiments/prange_ex.py
@@ -1,10 +1,18 @@
 import numpy as np
 import logging
-from isdclassic.methods import prange
+from isdclassic.methods.prange import Prange
 from isdclassic.utils.rectangular_codes_generation import generate_parity_matrix_nonsystematic_for_hamming
+from isdclassic.utils import rectangular_codes_hardcoded as rch
 
 logger = logging.getLogger(__name__)
 
+def ex_4_1():
+    h = rch._get_4_1_4_systematic_h()
+    s = np.array([0, 0, 1])
+    pr = Prange(h, s, 1)
+    e = pr.run()
+    print("H = \n{0}".format(h))
+    print("For s = {0}, e = {1}".format(s, e))
 
 def ex_7_4():
     h = generate_parity_matrix_nonsystematic_for_hamming(7, 4)
@@ -23,8 +31,9 @@ def ex_15_11():
 
 
 def main():
-    ex_7_4()
-    ex_15_11()
+    # ex_7_4()
+    # ex_15_11()
+    ex_4_1()
 
 
 if __name__ == "__main__":
