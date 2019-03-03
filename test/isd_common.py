55a19659b5bb8d28281897c23df6edf615120cf9
Simone Perriello <simone.perriello@protonmail.com>
2019-03-03 08:43:39 +0100

    Switched to file logger
    Also added printing of start time and elapsed time

--- test/isd_common.py
+++ test/isd_common.py
@@ -2,24 +2,48 @@ import logging
 import os
 import unittest
 import numpy as np
+import inspect
+import time
+from datetime import datetime
 
 
 class ISDTest(unittest.TestCase):
     MAX_N_SYNDROMES = 20
 
+    def setUp(self):
+        self._started_at = time.time()
+        now = datetime.now().strftime("%H:%M:%S")
+        print(now, end='->', flush=True)
+
+    def tearDown(self):
+        elapsed = time.time() - self._started_at
+        print('({:.2f}s)'.format(round(elapsed, 2)), end='', flush=True)
+
     @classmethod
     def setUpClass(cls):
         cls.logger = logging.getLogger(cls.__name__)
+        cur_file = inspect.getfile(cls)
+        cls.dirName = os.path.dirname(cur_file)
+        cls.fileName = os.path.splitext(os.path.basename(cur_file))[0]
+        cls.moduleName = os.path.splitext(cur_file)[0]
         if (os.getenv('LOG_LEVEL')):
-            stream_handler = logging.StreamHandler()
-            stream_handler_formatter = logging.Formatter(
-                '%(asctime)s %(levelname)-8s %(name)-12s %(funcName)-12s %(message)s'
-            )
-            stream_handler.setFormatter(stream_handler_formatter)
             logging_level = logging._nameToLevel.get(
                 os.getenv('LOG_LEVEL'), logging.info)
+            # stream_handler = logging.StreamHandler()
+            # stream_handler_formatter = logging.Formatter(
+            #     '%(asctime)s %(levelname)-8s %(name)-12s %(funcName)-12s %(message)s'
+            # )
+            # stream_handler.setFormatter(stream_handler_formatter)
+            log_file_name = os.path.join(cls.dirName, 'logs',
+                                         '{}.log'.format(cls.fileName))
+            log_fmt = ('%(asctime)s:%(module)s.%(funcName)s:%(levelname)s:'
+                       ' %(message)s')
+            formatter = logging.Formatter(log_fmt)
+            file_handler = logging.FileHandler(log_file_name)
+            file_handler.setFormatter(formatter)
             cls.logger.setLevel(logging_level)
-            cls.logger.addHandler(stream_handler)
+            # cls.logger.addHandler(stream_handler)
+            cls.logger.addHandler(file_handler)
 
     @classmethod
     def get_max_syndromes_errors(cls, syndromes, errors):
